511a74f7515c9a23ff06231be39b8ec62d25db72
JonathanDiz <you@example.com>
2022-12-17 19:07:37 -0300

    first commit

---
+++ app/MessageList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MessageList() {
+  return (
+    <div>
+      <h2>MessageList</h2>
+      <h2>MessageList</h2>
+      <h2>MessageList</h2>
+      <h2>MessageList</h2>
+    </div>
+  )
+}
+
+export default MessageList
